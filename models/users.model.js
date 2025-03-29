9fab5eaccaab05ccfd2896bdc5cc7216574342d9
Adrián Sanjuán Plaza <sanjuanplazaadrian@gmail.com>
2025-03-29 18:49:30 +0100

    actualizado el modelo de usuarios

--- models/users.model.js
+++ models/users.model.js
@@ -33,3 +33,58 @@ const userSchema = new mongoose.Schema({
 })
 
 const user = mongoose.model("user", userSchema)
+
+
+//crear usuario
+user.createUser = async(userData, result) => {
+    const newUser = new user(userData)
+    await newUser.save()
+    .then((data) => {
+        result(null, datos)
+    })
+    .catch((err) => {
+        result(err, null)
+    })
+}
+
+//listar todos los usuarios
+user.findAll = async(filter={}, result) => {
+    const datos = await user.find(filter)
+    if(datos && datos.lenght > 0){
+        result(null, datos)
+    }else{
+        result({"error":"No hay datos de usuarios"}, null)
+    }
+}
+
+//listar un usuario por su id
+user.findById = async(id, result) => {
+    const datos = await user.findById(id)
+    if(datos){
+        result(null, datos)
+    }else{
+        result({"error":"No hay datos"}, null)
+    }
+}
+
+user.updateUserById = async(id, userData, result) => {
+    await user.findByIdAndUpdate(id, userData, {runValidators:true, new:true})
+    .then((datosResult) => {
+        result(null, datosResult)
+    })
+    .catch((err) => {
+        result(err, null)
+    })
+}
+
+user.deleteUserById = async(id, result) => {
+    await user.findByIdAndDelete(id)
+    .then((datos) => {
+        result(null, datos)
+    })
+    .catch((err) => {
+        result(err, null)
+    })
+}
+
+module.exports = company
